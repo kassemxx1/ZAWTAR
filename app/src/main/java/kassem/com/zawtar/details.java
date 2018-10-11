fd9d56180efc0c5aee770777c17e10cf906c7ce9
kassemxx1 <abboudkassem1@gmail.com>
2018-10-11 13:37:04 +0300

    video confiration

--- app/src/main/java/kassem/com/zawtar/details.java
+++ app/src/main/java/kassem/com/zawtar/details.java
@@ -1,49 +1,91 @@
 package kassem.com.zawtar;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.Menu;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class details extends AppCompatActivity{
-    ArrayList<String> picturess;
+public class details extends AppCompatActivity {
+    ArrayList<String> picturess = new ArrayList<>();
+    ArrayList<String> videossss = new ArrayList<>();
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.detailscontroller);
-        ImageView detailImage=(ImageView)findViewById(R.id.imageView2);
-        TextView detailstitle=(TextView)findViewById(R.id.textView2);
-        TextView detailsDetails=(TextView)findViewById(R.id.textView3);
-        Bundle b=getIntent().getExtras();
-        String title=b.getString("title");
-        String details=b.getString("details");
-        String imagename=b.getString("imagename");
-        picturess=new ArrayList<>();
-        ArrayList<String> ddd=b.getStringArrayList("pics");
-        ArrayList<String> vvv=b.getStringArrayList("videos");
-       // picturess=ddd;
-        Log.d("ggggggg",""+ddd);
+        TextView detailstitle = (TextView) findViewById(R.id.textView2);
+        TextView detailsDetails = (TextView) findViewById(R.id.textView3);
+        Bundle b = getIntent().getExtras();
+        String title = b.getString("title");
+        String details = b.getString("details");
+        String imagename = b.getString("imagename");
+        ArrayList<String> ddd = b.getStringArrayList("pics");
+        ArrayList<String> vvv = b.getStringArrayList("videos");
+        if (ddd == null) {
+            picturess.add(imagename);
+        } else {
+            picturess = ddd;
+        }
+
         detailstitle.setText(title);
         detailsDetails.setText(details);
-        new DownloadImageTask((ImageView) detailImage)
-                .execute(imagename);
-        ListView listimage=findViewById(R.id.PICS);
-        picturesadapter aaa=new picturesadapter();
-       listimage.setAdapter(aaa);
+
+        ListView listimage = (ListView) findViewById(R.id.PICS);
+        listimage.setOnTouchListener(new ListView.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int action = event.getAction();
+                switch (action) {
+                    case MotionEvent.ACTION_DOWN:
+                        // Disallow ScrollView to intercept touch events.
+                        v.getParent().requestDisallowInterceptTouchEvent(true);
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+                        // Allow ScrollView to intercept touch events.
+                        v.getParent().requestDisallowInterceptTouchEvent(false);
+                        break;
+                }
+
+                // Handle ListView touch events.
+                v.onTouchEvent(event);
+                return true;
+            }
+        });
+        picturesadapter aaa = new picturesadapter();
+        listimage.setAdapter(aaa);
+        String frameVideo = "<html><body>Video From YouTube<br><iframe width=\"420\" height=\"315\" src=\"https://www.youtube.com/embed/47yJ2XCRLZs\" frameborder=\"0\" allowfullscreen></iframe></body></html>";
+
+        WebView vp=(WebView)findViewById(R.id.videolayout);
+        vp.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                return false;
+            }
+        });
+        WebSettings webSettings = vp.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        vp.loadData(frameVideo, "text/html", "utf-8");
+        vp.setWebViewClient();
 
     }
 
-    class picturesadapter extends BaseAdapter{
+    class picturesadapter extends BaseAdapter {
 
         @Override
         public int getCount() {
@@ -52,7 +94,7 @@ public class details extends AppCompatActivity{
 
         @Override
         public Object getItem(int position) {
-           return null;
+            return null;
         }
 
 
@@ -60,14 +102,16 @@ public class details extends AppCompatActivity{
             return 0;
         }
 
-       @Override
+        @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            View view=getLayoutInflater().inflate(R.layout.imagescell,null);
-            ImageView pic=view.findViewById(R.id.pictures);
+            View view = getLayoutInflater().inflate(R.layout.imagescell, null);
+            ImageView pic = view.findViewById(R.id.pictures);
             new DownloadImageTask((ImageView) pic)
                     .execute(picturess.get(position));
 
-            return null;
+            return view;
         }
     }
+
+
 }
